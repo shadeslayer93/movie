d468a22e3dfde203a3d9f662af37f6ffb0e598d5
Siddharth Nair <sid_shadeslayer@Siddharths-MacBook-Air.local>
2015-08-06 12:53:13 +0530

    Make indentation changes.

--- media.py
+++ media.py
@@ -3,12 +3,12 @@ import webbrowser
 class Movie():
     """This is the Movie template based on which other 
        instances are created !! """
-       
+
     def __init__(self, movie, trailer, poster, story):
-    	self.title = movie
-		self.trailer_youtube_url = trailer
-		self.poster_image_url = poster
-		self.story_line = story
+        self.title = movie
+        self.trailer_youtube_url = trailer
+        self.poster_image_url = poster
+        self.story_line = story
 
     def show_trailer(self):
     	webbrowser.open(self.trailer_youtube_url)
